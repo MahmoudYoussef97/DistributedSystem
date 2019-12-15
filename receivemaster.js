6100168a19c0e157ebd073555901ef25dcc8d372
Mahmoud Essam <mahmoudessam1456@gmail.com>
2019-12-15 06:22:19 +0200

    slave

---
+++ receivemaster.js
@@ -0,0 +1,119 @@
+const amqp = require("amqplib");
+
+var express = require("express");
+const r = require("rethinkdb");
+
+let dbConnection;
+var app = express();
+
+const connect = async function connectFn() {
+  dbConnection = await r.connect({
+    host: "localhost",
+    port: 28015,
+    db: "company"
+  });
+};
+const getCount = async function getCount(table) {
+  try {
+    const result = await r
+      .table(table)
+      .count()
+      .run(dbConnection);
+
+    const date = await result;
+    console.log(date);
+    if (date.length === 0) throw new Error("Not Found");
+
+    return date;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getSum = async function getSum(table, start, end) {
+  try {
+    const result = await r
+      .table(table)
+      .slice(start, end)
+      .sum("salary")
+      .run(dbConnection);
+
+    const date = await result;
+    console.log(date);
+    if (date.length === 0) throw new Error("Not Found");
+
+    return date;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const close = async function closeFn() {
+  return dbConnection.close();
+};
+
+async function doSum(first, last) {
+  const c = await connect();
+  const sum = await getSum("salary", first, last);
+  await close();
+  return sum;
+}
+async function doCount() {
+  const c = await connect();
+  const count = await getCount("salary");
+  await close();
+  return count;
+}
+
+const main = async () => {
+  const opt = {
+    credentials: require("amqplib").credentials.plain("test", "test")
+  };
+  const amqpUrl = "amqp://192.168.1.7";
+  const connection = await amqp.connect(amqpUrl, opt);
+  const channel = await connection.createChannel();
+
+  const queue = "MasterA";
+  channel.assertQueue(queue, { durable: false });
+
+  channel.consume(
+    queue,
+    async message => {
+      console.log(message && message.content.toString());
+
+      if (message.content.toString() == "count") {
+        const count = await doCount();
+        channel.sendToQueue(
+          message.properties.replyTo,
+          Buffer.from(count.toString()),
+          {
+            correlationId: message.properties.correlationId
+          }
+        );
+      } else {
+        const rangestr = message.content.toString();
+        const range = rangestr.split(" ");
+        const first = parseInt(range[0], 10);
+        const last = parseInt(range[1], 10);
+        const sum = await doSum(first, last);
+        channel.sendToQueue(
+          message.properties.replyTo,
+          Buffer.from(sum.toString()),
+          {
+            correlationId: message.properties.correlationId
+          }
+        );
+      } /*
+      channel.sendToQueue(message.properties.replyTo, Buffer.from("5"), {
+        correlationId: message.properties.correlationId
+      });*/
+    },
+    { noAck: false }
+  );
+
+  process.on("exit", _ => {
+    connection.close();
+  });
+};
+
+main().catch(console.error);
---
+++ sendmonitor.js
@@ -0,0 +1,31 @@
+const amqp = require("amqplib");
+
+const main = async () => {
+  const opt = {
+    credentials: require("amqplib").credentials.plain("test", "test")
+  };
+  const amqpUrl = "amqp://192.168.1.7";
+  const connection = await amqp.connect(amqpUrl, opt);
+  const channel = await connection.createChannel();
+
+  const queue = "Monitor";
+
+  channel.assertQueue(queue, { durable: false });
+
+  const interval = setInterval(_ => {
+    const time = new Date().toJSON();
+    const cpuUsage = Math.floor(Math.random() * 100);
+    const message = `[${time}] - Server: A - CPU: ${cpuUsage}%`;
+
+    channel.sendToQueue(queue, Buffer.from(message));
+
+    console.info(message);
+  }, 5000);
+
+  process.on("exit", _ => {
+    connection.close();
+    clearInterval(interval);
+  });
+};
+
+main().catch(console.error);
